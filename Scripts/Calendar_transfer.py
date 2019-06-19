1a03cce8fc07410cecc6b99f776ce0ab9c8bf1f1
Shane P <shane.pullens@hotmail.com>
2019-06-19 20:50:59 +0200

    Added try except for lack of internet connection

--- Scripts/Calendar_transfer.py
+++ Scripts/Calendar_transfer.py
@@ -54,7 +54,10 @@ def getCredentials():
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+            try:
+                creds.refresh(Request())
+            except:
+                print("Error! No internet connection.")
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'Data/credentials.json', SCOPES)
